38f79f9f610d1a7284bbe1e11232902a121a1b76
Nuthan30 <nuthan30062004@gmail.com>
2025-09-13 19:29:13 +0530

    Add files via upload

---
+++ list_slicing.py
@@ -0,0 +1,6 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+first_five=list[:5]
+reversed_element=first_five[::-1]
+print(f"Original list: {list}")
+print(f"Extracted first five elements: {first_five}")
+print(f"Reversed extracted elements: {reversed_element}")
